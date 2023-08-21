dbf577fb658cbedfa9690e5c3b2bf7340c64d161
MaazDanish <dk599318@gmail.com>
2023-08-21 20:24:43 +0530

    UserInterface Done

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 let addProductButton = document.querySelector('#add-product-btn');
 let priceList = document.querySelector('#priceList');
 
-//  Getting all data and showing it on screen
-window.addEventListener('DOMContentLoaded', () => {
-	axios.get('https://crudcrud.com/api/2ef5e2ab147243eb8cbbec09f2492d12/listProduct')
-		.then((res) => {
-			for (var i = 0; i < res.data.length; i++) {
-				displayOnScreen(res.data[i]);
-			}
-		})
-		.catch((err) => console.log(err));
-})
-
 // saving data in crud crud using object
 function saveData(event) {
 	event.preventDefault();
@@ -22,7 +11,7 @@ function saveData(event) {
 		price
 	}
 
-	axios.post('https://crudcrud.com/api/2ef5e2ab147243eb8cbbec09f2492d12/listProduct', productStore)
+	axios.post('https://crudcrud.com/api/7c27d498f402443aa958cb05d852ee41/listProduct', productStore)
 		.then((res) => displayOnScreen(res.data))
 		.catch((err) => console.log(err));
 
@@ -30,6 +19,18 @@ function saveData(event) {
 	event.target.price.value = '';
 
 }
+
+//  Getting all data and showing it on screen
+
+window.addEventListener('DOMContentLoaded', () => {
+	axios.get('https://crudcrud.com/api/7c27d498f402443aa958cb05d852ee41/listProduct')
+		.then((res) => {
+			for (var i = 0; i < res.data.length; i++) {
+				displayOnScreen(res.data[i]);
+			}
+		})
+		.catch((err) => console.log(err));
+})
 
 //  to display on screen
 function displayOnScreen(productStore) {
@@ -41,7 +42,8 @@ function displayOnScreen(productStore) {
 
 		//  creating li element for storing data
 		let li = document.createElement('li');
-		li.textContent = productStore.name + "  -- " + productStore.price;
+		li.className = 'margin-top color-of-text';
+		li.textContent = `Product Name - ${productStore.name} || Product Cost - ${productStore.price} `;
 
 		// storing total amount of product
 		let costPlace = document.getElementById('costPlace');
@@ -51,11 +53,12 @@ function displayOnScreen(productStore) {
 		let deleteButton = document.createElement('input');
 		deleteButton.type = 'button';
 		deleteButton.value = 'Delete';
+		deleteButton.className = 'btn btn-outline-danger float-end';
 
 
 		let id = productStore._id;
 		deleteButton.onclick = () => {
-			axios.delete('https://crudcrud.com/api/2ef5e2ab147243eb8cbbec09f2492d12/listProduct/' + id)
+			axios.delete('https://crudcrud.com/api/7c27d498f402443aa958cb05d852ee41/listProduct/' + id)
 				.then(() => {
 					value = Number(costPlace.textContent) - Number(productStore.price)
 					costPlace.textContent = value;
@@ -63,6 +66,7 @@ function displayOnScreen(productStore) {
 				.catch((error) => console.log(error));
 			productList.removeChild(li);
 		};
+		// Appending the elements in parent element
 		li.appendChild(deleteButton);
 		productList.appendChild(li);
 	}
